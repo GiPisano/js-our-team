00d038b4407ae0ecc60b838489c35b4216cc5e07
GiPisano <pisanogiacomo9@gmail.com>
2024-01-24 09:35:01 +0100

    add correction for loop

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 const persona = document.getElementById('persona')
 console.log(team)
 
-for(person in team){
+for(let person in team){
     let datiperson = team[person];
     createCard(persona, datiperson.image, datiperson.name, datiperson.role)
-  };
+};
    
+
 function createCard(element, img, name, role){
-    element.innerHTML += `<div class="col col-lg-4 col-card">
+    element.innerHTML += `<div class="col col-lg-4 col-md-6 col-card">
     <div class="card" style="width: 20rem;">
         <img src="./img/${img}" class="card-img-top">
         <div class="card-body">
